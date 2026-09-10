e2ed80d556675d1b98108903e9847ded5332651b
evgnskv <evgnskv@pm.me>
2026-09-10 11:37:04 +0900

    Add keyboard shortcuts section

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -94,7 +94,7 @@
 
         # Mission Control
         spaces.spans-displays = true;
-        #universalaccess.reduceMotion = true;
+        universalaccess.reduceMotion = true;
         NSGlobalDomain.NSWindowShouldDragOnGesture = true;
         NSGlobalDomain.NSAutomaticWindowAnimationsEnabled = false;
         dock.expose-group-apps = true;
@@ -120,6 +120,12 @@
         dock.wvous-bl-corner = 1;
         dock.wvous-br-corner = 1;
 
+        # Clock
+        menuExtraClock.FlashDateSeparators = true;
+        menuExtraClock.Show24Hour = true;
+        menuExtraClock.ShowDate = 2;
+        menuExtraClock.ShowDayOfWeek = false;
+
         # Typing
         NSGlobalDomain."com.apple.keyboard.fnState" = true;
         NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
@@ -129,11 +135,21 @@
         NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
         NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
 
-        # Clock
-        menuExtraClock.FlashDateSeparators = true;
-        menuExtraClock.Show24Hour = true;
-        menuExtraClock.ShowDate = 2;
-        menuExtraClock.ShowDayOfWeek = false;
+        # Keyboard shortcuts
+        CustomUserPreferences = {
+          "com.apple.symbolichotkeys" = {
+            AppleSymbolicHotKeys = {
+              # Free Ctrl+Space from input source switching
+              "61" = {
+                enabled = false;
+                value = {
+                  parameters = [ 32 49 262144 ];
+                  type = "standard";
+                };
+              };
+            };
+          };
+        };
 
       };
 
